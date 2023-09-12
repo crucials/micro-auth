8ff47a1b2fda91aead37d991390f845b0dc94f87
mournful <devoting@yandex.ru>
2023-09-12 08:04:04 +0500

    Added graphql support for `CurrentAccount` decorator

--- src/auth-base/decorators/current-account.ts
+++ src/auth-base/decorators/current-account.ts
@@ -1,6 +1,9 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common'
+import { GqlExecutionContext } from '@nestjs/graphql'
 
 export const CurrentAccount = createParamDecorator((data, context : ExecutionContext) => {
     const request = context.switchToHttp().getRequest()
+        || GqlExecutionContext.create(context).getContext().req
+
     return request.account
 })
